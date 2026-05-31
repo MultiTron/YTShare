9d088b9f0027c6c009268eb86b6696b32c6ea9e4
Ivaylo Iliev <iliev_evn@hotmail.com>
2026-05-31 13:00:33 +0300

    feat: add Krossbow STOMP and FCM dependencies to Android app

--- YTShare.Android/app/build.gradle.kts
+++ YTShare.Android/app/build.gradle.kts
@@ -101,6 +101,11 @@ dependencies {
     // Firebase
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.auth)
+    implementation("com.google.firebase:firebase-messaging-ktx")
+
+    // Krossbow STOMP client
+    implementation("org.hildan.krossbow:krossbow-stomp-core:7.0.0")
+    implementation("org.hildan.krossbow:krossbow-websocket-okhttp:7.0.0")
 
     // Coroutines
     implementation(libs.kotlinx.coroutines.play.services)
