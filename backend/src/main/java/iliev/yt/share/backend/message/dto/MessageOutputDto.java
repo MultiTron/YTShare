4128434f20648a0635eccd35ce81aed5c84c75e7
Ivaylo Iliev <iliev_evn@hotmail.com>
2026-05-31 12:49:06 +0300

    feat: add websocket dependency and createdAt to MessageOutputDto

--- backend/src/main/java/iliev/yt/share/backend/message/dto/MessageOutputDto.java
+++ backend/src/main/java/iliev/yt/share/backend/message/dto/MessageOutputDto.java
@@ -3,6 +3,7 @@ package iliev.yt.share.backend.message.dto;
 import iliev.yt.share.backend.chat.dto.ChatOutputDto;
 import iliev.yt.share.backend.message.enums.DeliveryStatus;
 import iliev.yt.share.backend.user.dto.UserOutputDto;
+import java.util.Date;
 import java.util.UUID;
 
 public record MessageOutputDto(
@@ -10,6 +11,7 @@ public record MessageOutputDto(
         String content,
         DeliveryStatus status,
         ChatOutputDto chat,
-        UserOutputDto sender
+        UserOutputDto sender,
+        Date createdAt
 ) {
 }
